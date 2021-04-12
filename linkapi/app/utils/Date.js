8cc96791fee1830112e48e5b3ae4c579b02c176c
Rodrigo Andrade <datarodrigoandrade@gmail.com>
2021-04-12 01:46:38 -0300

    Temporarily fixed TZ problem when running application inside a container.

--- linkapi/app/utils/Date.js
+++ linkapi/app/utils/Date.js
@@ -1,3 +1,5 @@
+const IS_RUNNING_CONTAINER = require("../constants/App");
+
 const moment = require("moment");
 require("moment/min/locales");
 
@@ -8,7 +10,7 @@ const DateUtils = {
         return format ? moment().format(format) : moment().format();
     },
     getYesterdayBrazilianFormat(format){
-        return moment(this.getTodayBrazilianFormat()).subtract('days', 1).format(format); 
+        return moment(this.getTodayBrazilianFormat()).subtract('days', IS_RUNNING_CONTAINER == "yes" ? 2 : 1).format(format); 
     }
 };
 
